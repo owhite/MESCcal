e44d2fc67565b6aec57c8011930d7bb26dff6fb6
Owen White <smouldering.dog@gmail.com>
2023-11-28 08:38:53 -0500

    made variable_class list

--- mescal2.py
+++ mescal2.py
@@ -4,7 +4,7 @@
 
 import sys
 import math
-import re
+import FirstTab
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QPlainTextEdit, QTabWidget, QVBoxLayout, QGridLayout, QGroupBox
 from PyQt5.QtCore import Qt
@@ -23,234 +23,48 @@ class Mescal(QtWidgets.QMainWindow):
         # sets the tabWidget as the central widget inside the QMainWindow
         self.setCentralWidget(self.tabWidget)
 
-        self.payload = ''
-
         ### Status Bar ###
         self.setStatusBar( QtWidgets.QStatusBar(self) )
         self.statusText = QtWidgets.QLabel(self)
         self.statusBar().addWidget( self.statusText )
         
         ### Tabs ###
-        self.tab_first = FirstTab()
+
+        self.tab_first = FirstTab.FirstTab(self.port, self.statusText)
         self.tabWidget.addTab(self.tab_first,"Port")
-        self.tab_second = SecondTab()
-        self.tabWidget.addTab(self.tab_second,"Tab 2")
+        self.tab_second = SecondTab(self.port, self.statusText)
+        self.tabWidget.addTab(self.tab_second,"Motor parameters")
 
-    def keyPressEvent(self, e):
-        print("event", e)
-        if e.key()  == Qt.Key_Return :
-            self.sendFromPort('beep')
-            print(' return')
-        elif e.key() == Qt.Key_Enter :   
-            print(' enter')
 
-class FirstTab(QtWidgets.QMainWindow):
+class SecondTab(QtWidgets.QMainWindow): # motor parameters
 
-    def __init__(self):
+    def __init__(self, port, statusText):
         super().__init__()
+        self.port = port
+        self.statusText = statusText
         self.initUI()
 
     def initUI(self):
 
-        layout = QGridLayout()
-        self.setLayout(layout)
+        self.setCentralWidget( QtWidgets.QWidget(self) )
 
-        self.port.close()
+        layout = QtWidgets.QFormLayout( self.centralWidget() )
 
-        groupbox = QGroupBox("GroupBox Example1")
-        layout.addWidget(groupbox)
+        self.pb1 = QtWidgets.QPushButton("One")
+        self.lineEdit1 = QtWidgets.QLineEdit()
+        self.pb2 = QtWidgets.QPushButton("Two")
+        self.lineEdit2 = QtWidgets.QLineEdit()
+        self.pb3 = QtWidgets.QPushButton("Three")
+        self.lineEdit3 = QtWidgets.QLineEdit()
 
-        ### Tool Bar ###
-        self.toolBar = ToolBar(self)
-        self.addToolBar(self.toolBar)
+        layout.addRow(self.pb1, self.lineEdit1)
+        layout.addRow(self.pb2, self.lineEdit2)
+        layout.addRow(self.pb3, self.lineEdit3)
 
-        # Layout manager QVBox (vertical)
-        vbox = QVBoxLayout()
-        groupbox.setLayout(vbox)
+        self.pb1.clicked.connect(self.pb1Clicked)
 
-        ### Signal Connect ###
-        self.toolBar.portOpenButton.clicked.connect(self.portOpen)
-        self.toolBar.portRefreshButton.clicked.connect(self.portRefresh)
-
-    def portOpen(self, flag):
-        if flag:
-            self.port.setBaudRate( self.toolBar.baudRate() )
-            self.port.setPortName( self.toolBar.portName() )
-            self.port.setDataBits( 8 )
-            self.port.setParity( 0 ) 
-            self.port.setStopBits( 0 ) 
-            self.port.setFlowControl( 0 ) 
-            r = self.port.open(QtCore.QIODevice.ReadWrite)
-            if not r:
-                self.statusText.setText('Port open: error')
-                self.toolBar.portOpenButton.setChecked(False)
-                # self.toolBar.serialControlEnable(True)
-            else:
-                self.statusText.setText('Port opened')
-                # self.toolBar.serialControlEnable(False)
-        else:
-            self.port.close()
-            self.statusText.setText('Port closed')
-            # self.toolBar.serialControlEnable(True)
-        
-    def portRefresh(self, flag):
-        self.port.close() # bad idea? 
-        self.portOpen(True)
-        self.statusText.setText('Port refresh')
-        l = []
-        count = 0
-        for port in [ port.portName() for port in QSerialPortInfo().availablePorts() ]:
-            l.append(port)
-            if 'cu.usbmodem' in port:
-                l[count] = l[0]
-                l[0] = port
-            count = count + 1
-
-        self.toolBar.portNames.clear()       
-        self.toolBar.portNames.addItems( l ) 
-
-    def readFromPort(self):
-        data = self.port.readAll().data().decode()
-        # strip vt100 chars
-        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-        data = ansi_escape.sub('', data)
-        data = re.sub('\| ', '\t', data)
-        self.payload += data 
-        if len(data) > 0:
-            # self.serialDataView.appendSerialText( QtCore.QTextStream(data).readAll(), QtGui.QColor(255, 0, 0) )
-            self.serialDataView.appendSerialText( data, QtGui.QColor(0, 0, 0))
-
-    def sendFromPort(self, text):
-        text = text + '\r\n'
-        self.port.write( text.encode() )
-        self.payload = ''
-        self.serialDataView.appendSerialText( text, QtGui.QColor(0, 0, 255) )
-
-
-class SecondTab(QtWidgets.QWidget):
-
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-
-        self.setWindowTitle("Groupbox")
-        layout = QGridLayout()
-        self.setLayout(layout)
-
-        groupbox = QGroupBox("GroupBox Example2")
-        # groupbox.setCheckable(True)
-        layout.addWidget(groupbox)
-
-        # Layout manager QVBox (vertical)
-        vbox = QVBoxLayout()
-        groupbox.setLayout(vbox)
-
-        # radiobutton = QRadioButton("Radiobutton 1")
-        # vbox.addWidget(radiobutton)
-
-        # textEdit_input = QTextEdit()
-        # vbox.addWidget(textEdit_input)
-
-        # textEdit_output = QTextEdit()
-        # vbox.addWidget(textEdit_output)
-
-class SerialDataView(QtWidgets.QWidget):
-    def __init__(self, parent):
-        super(SerialDataView, self).__init__(parent)
-        self.serialData = QtWidgets.QTextEdit(self)
-        self.serialData.setReadOnly(True)
-        self.serialData.setFontFamily('Courier New')
-        self.serialData.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        self.setLayout( QtWidgets.QGridLayout(self) )
-        self.layout().addWidget(self.serialData, 0, 0, 1, 1)
-        self.layout().setContentsMargins(2, 2, 2, 2)
-        
-    def appendSerialText(self, appendText, color):
-        self.serialData.moveCursor(QtGui.QTextCursor.End)
-        self.serialData.setFontFamily('Courier New')
-        self.serialData.setTextColor(color)
-        self.serialData.insertPlainText(appendText)
-        self.serialData.moveCursor(QtGui.QTextCursor.End)
-
-class SerialSendView(QtWidgets.QWidget):
-
-    serialSendSignal = QtCore.pyqtSignal(str)
-    def __init__(self, parent):
-        super(SerialSendView, self).__init__(parent)
-
-        self.sendData = QtWidgets.QTextEdit(self)
-        self.sendData.setAcceptRichText(False)
-        self.sendData.setMaximumHeight(32)
-        self.sendData.setSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-
-        self.sendButton = QtWidgets.QPushButton('Send')
-        self.sendButton.clicked.connect(self.sendButtonClicked)
-        self.sendButton.setSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Preferred)
-        
-        self.setLayout( QtWidgets.QHBoxLayout(self) )
-        self.layout().addWidget(self.sendData)
-        self.layout().addWidget(self.sendButton)
-        self.layout().setContentsMargins(3, 3, 3, 3)
-
-
-    def sendButtonClicked(self):
-        self.serialSendSignal.emit( self.sendData.toPlainText() )
-        self.sendData.clear()
-
-class ToolBar(QtWidgets.QToolBar):
-    def __init__(self, parent):
-        super(ToolBar, self).__init__(parent)
-        
-        self.portOpenButton = QtWidgets.QPushButton('Open')
-        self.portOpenButton.setCheckable(True)
-        self.portOpenButton.setMinimumHeight(32)
-
-        self.portRefreshButton = QtWidgets.QPushButton('Refresh')
-        self.portRefreshButton.setCheckable(True)
-        self.portRefreshButton.setMinimumHeight(32)
-
-        self.portNames = QtWidgets.QComboBox(self)
-        l = []
-        count = 0
-        for port in [ port.portName() for port in QSerialPortInfo().availablePorts() ]:
-            l.append(port)
-            if 'cu.usbmodem' in port:
-                l[count] = l[0]
-                l[0] = port
-            count = count + 1
-
-        self.portNames.addItems( l )
-        self.portNames.setMinimumHeight(32)
-
-        self.baudRates = QtWidgets.QComboBox(self)
-        self.baudRates.addItems([
-            '110', '300', '600', '1200', '2400', '4800', '9600', '14400', '19200', '28800', 
-            '31250', '38400', '51200', '56000', '57600', '76800', '115200', '128000', '230400', '256000', '921600'
-        ])
-        self.baudRates.setCurrentText('115200')
-        self.baudRates.setMinimumHeight(30)
-
-        self.addWidget( self.portOpenButton )
-        self.addWidget( self.portRefreshButton )
-        self.addWidget( self.portNames )
-        self.addWidget( self.baudRates )
-
-    def serialControlEnable(self, flag):
-        self.portNames.setEnabled(flag)
-        self.baudRates.setEnabled(flag)
-        self.dataBits.setEnabled(flag)
-        self._parity.setEnabled(flag)
-        self.stopBits.setEnabled(flag)
-        self._flowControl.setEnabled(flag)
-        
-    def baudRate(self):
-        return int(self.baudRates.currentText())
-
-    def portName(self):
-        return self.portNames.currentText()
+    def pb1Clicked(self):
+        print('Your name: ' + self.lineEdit1.text())
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
