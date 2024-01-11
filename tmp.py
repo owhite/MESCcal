494bca58c4c1d2bd71f8cbb6ca36678e72245ec4
Owen White <smouldering.dog@gmail.com>
2024-01-11 14:45:25 -0500

    more testing

--- tmp.py
+++ tmp.py
@@ -9,36 +9,52 @@ from functools import partial
 class Test(QWidget):
     def __init__(self):
         super().__init__()
+
+        # Set the initial position of the window
+        self.move(100, 100)  # Set x=100, y=100
+
         self.resize(200, 100)
         self.layout = QHBoxLayout(self)
-        label = QLabel('QToolButton Popup')
 
         self.tools = []
         self.tool_index = 0
         self.toolButtonHighlightIndex = 0
         self.toolButtonButtons = {}
 
-        tool = self.createTool("B1", "thing1")
+        tool = self.createTool("req_adc1", "12")
         self.layout.addWidget(tool)
         self.tools.append(tool)
-        self.tool_index = self.tool_index + 1
+        tool.clicked.connect(self.openTool)
+        tool.mousePressEvent = self.toolButtonMousePressEvent 
 
-        tool = self.createTool("B2", "thing2")
+        # self.tool_index = self.tool_index + 1
+
+        tool = self.createTool("curr_max", "40.0")
         self.layout.addWidget(tool)
         self.tools.append(tool)
-        self.tool_index = self.tool_index + 1
+        tool.clicked.connect(self.openTool)
+        tool.mousePressEvent = self.toolButtonMousePressEvent 
+
+        # self.tool_index = self.tool_index + 1
+        self.navigateTools(0)
         
-        self.layout.addWidget(label)
+    def toolButtonMousePressEvent(self, event):
+        sender_button = self.sender()
+        print(f"QToolButton clicked by mouse {self}")
+
+    def onToolButtonClick2(self):
+        sender_button = self.sender()
+        if sender_button:
+            print(f"Opened: {sender_button.text()}")
 
     def navigateTools(self, direction):
         self.tool_index = (self.tool_index + direction) % len(self.tools)
 
-        print(self.tool_index)
         count = 0
         for t in self.tools:
             if count == self.tool_index:
-                t.setStyleSheet("background-color: yellow")
-                t.menu().setStyleSheet("background-color: red")
+                t.setStyleSheet("background-color: white")
+                t.menu().setStyleSheet("background-color: white")
             else:
                 t.setStyleSheet("")
                 t.menu().setStyleSheet("")
@@ -54,7 +70,6 @@ class Test(QWidget):
 
         # Show the layout when pressing the Enter key
         elif key == Qt.Key_Enter or key == Qt.Key_Return:
-            print("OPEN")
             self.openTool()
 
         # Highlight the QToolButton in the current row using up/down arrows
@@ -64,45 +79,61 @@ class Test(QWidget):
             print("DOWN")
 
     def openTool(self):
-        print("show")
-        self.toolButtonHighlightIndex = 0
+        print("HERE")
         t = self.tools[self.tool_index]
-        t.menu().setStyleSheet("")
-        self.toolButtonHightlight(t)
+        t.setStyleSheet("background-color: white")
+        t.menu().setStyleSheet("background-color: white")
+        self.toolButtonHighlightIndex = 0
+        self.toolButtonHighlight(t.menu())
         t.showMenu()
 
     def createTool(self, button_text, layout_label):
         tool = QToolButton() # confusing because this code calls this a "tool", but ¯\_(ツ)_/
-        tool.setText(button_text)
+
+        tool.setText(button_text + '\n' + layout_label)
         tool.setPopupMode(QToolButton.InstantPopup)
 
         widget = QWidget()
+        widget.setStyleSheet("background-color: white;" "border :1px solid black;")
+        widget_menu = QMenu(tool)
         widget_layout = QGridLayout(widget)
         widget_label = QLabel(layout_label)
+        widget_layout.addWidget(widget_label, 0, 0, 1, 4)
 
         # Adjust spacing between buttons
         widget_layout.setHorizontalSpacing(4)
         widget_layout.setVerticalSpacing(4)
 
-        widget.setStyleSheet("background-color: white;" "border :1px solid black;")
-
         buttons = []
-        for i in range(3):
-            for j in range(3):
-                num = i * 3 + j + 1
-                button = QPushButton(f' {num} ')
-                buttons.append(button)
-                button.setStyleSheet("background-color: white;" "border :1px solid black;")
-                button.clicked.connect(partial(self.onToolButtonClick, num, widget_label))
-                widget_layout.addWidget(button, i+1, j+1)
+        keys = [
+            '7', '8', '9', '<<',
+            '4', '5', '6', 'cancel',
+            '1', '2', '3', '',
+            '0', '.', 'enter', ''
+        ]
 
-        widget_layout.addWidget(widget_label, 0, 0, 1, 3)
+        row = 1
+        col = 0
+
+        for key in keys:
+            button = QPushButton(key, self)
+            button.clicked.connect(partial(self.onToolButtonClick, key, button_text, widget_label))
+            buttons.append(button)
+            if key == '':
+                button.hide()
+            elif key == 'enter':
+                widget_layout.addWidget(button, row, col, 1, 2)
+            else:
+                widget_layout.addWidget(button, row, col)
+            col += 1
+            if col > 3:
+                col = 0
+                row += 1
 
         widget_action = QWidgetAction(tool)
         widget_action.setDefaultWidget(widget)
 
-        widget_menu = QMenu(tool)
-        self.toolButtonButtons[tool] = buttons
+        self.toolButtonButtons[widget_menu] = buttons
 
         widget_menu.addAction(widget_action)
         widget_menu.installEventFilter(self)
@@ -114,32 +145,70 @@ class Test(QWidget):
     def eventFilter(self, obj, event):
         if event.type() == QEvent.KeyPress:
             key = event.key()
-            print(f'Key pressed in submenu: {key} :: {obj}')
 
             if key == Qt.Key_Up:
-                print("UP")
+                if self.toolButtonHighlightIndex == 3:
+                    self.toolButtonHighlightIndex = 14
+                elif self.toolButtonHighlightIndex == 7:
+                    self.toolButtonHighlightIndex = 3
+                elif self.toolButtonHighlightIndex == 14:
+                    self.toolButtonHighlightIndex = 7
+                else:
+                    self.toolButtonHighlightIndex = (self.toolButtonHighlightIndex - 4) % len(self.toolButtonButtons[obj])
+                self.toolButtonHighlight(obj)
             elif key == Qt.Key_Down:
-                print("DOWN")
+                if self.toolButtonHighlightIndex == 7:
+                    self.toolButtonHighlightIndex = 14
+                elif self.toolButtonHighlightIndex == 14:
+                    self.toolButtonHighlightIndex = 3
+                else:
+                    self.toolButtonHighlightIndex = (self.toolButtonHighlightIndex + 4) % len(self.toolButtonButtons[obj])
+                self.toolButtonHighlight(obj)
             elif key == Qt.Key_Left:
-                print("LEFT")
+                self.toolButtonHighlightIndex = (self.toolButtonHighlightIndex - 1) % len(self.toolButtonButtons[obj])
+                self.toolButtonHighlight(obj)
             elif key == Qt.Key_Right:
-                print("RIGHT")
+                self.toolButtonHighlightIndex = (self.toolButtonHighlightIndex + 1) % len(self.toolButtonButtons[obj])
+                self.toolButtonHighlight(obj)
             elif key == Qt.Key_Enter or key == Qt.Key_Return:
-                print("TOOL ENTER")
-                obj.showMenu()
-                
+                b = self.toolButtonButtons[obj][self.toolButtonHighlightIndex]
+                b.click()
+                return True
 
         return super().eventFilter(obj, event)
 
-    def toolButtonHightlight(self, obj):
-        print(obj)
-        b = self.toolButtonButtons[obj][self.toolButtonHighlightIndex]
-        b.setStyleSheet("background-color: grey;" "border :1px solid black;")
+    def toolButtonHighlight(self, obj):
+        for n, b in enumerate(self.toolButtonButtons[obj]):
+            if n == self.toolButtonHighlightIndex:
+                b.setStyleSheet("background-color: grey;" "border :1px solid black;")
+            else:
+                b.setStyleSheet("background-color: white;" "border :1px solid black;")
 
-    def onToolButtonClick(self, key, label):
-        print(f"widget_button clicked! {key} :: {label.text()}")
-        if type(key) is int:
-            label.setText(label.text() + str(key))
+    def onToolButtonClick(self, key, value, label):
+        button = self.sender() 
+        if button: # a lot of ifs!
+            action = button.parentWidget()
+            if action:
+                menu = action.parentWidget()
+                if menu:
+                    tool = menu.parentWidget()
+        try:
+            int_value = int(key)
+            label.setText(label.text() + str(int_value))
+            print(label.text())
+        except:
+            if (key) == 'cancel':
+                menu.hide()
+            if (key) == '<<':
+                s = label.text()
+                label.setText(s[:-1])
+            if (key) == '.':
+                label.setText(label.text() + key)
+            if (key) == 'enter':
+                tool.setText(value + '\n' + label.text())
+                print(f"set {value} {label.text()}")
+                menu.hide()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- tmp2.py
+++ tmp2.py
@@ -1,160 +1,63 @@
 #!/usr/bin/env python3
 
-def createToolButton(self, button_text, layout_label):
-    tool_button = QToolButton()
-    tool_button.setText(button_text)
-    tool_button.setPopupMode(QToolButton.InstantPopup)
-
-    widget = QWidget()
-    widget_layout = QGridLayout(widget)
-    widget_label = QLabel(layout_label)
-
-    # Adjust spacing between buttons
-    widget_layout.setHorizontalSpacing(2)
-    widget_layout.setVerticalSpacing(2)
-
-    # Create a grid of buttons
-    for i in range(3):
-        for j in range(3):
-            button = QPushButton(f'{i * 3 + j + 1}')
-            button.setStyleSheet("background-color: red")
-            button.clicked.connect(self.onWidgetButtonClick)
-            widget_layout.addWidget(button, i+1, j+1)
-
-    widget_layout.addWidget(widget_label, 0, 0, 1, 3)
-
-    widget_action = QWidgetAction(tool_button)
-    widget_action.setDefaultWidget(widget)
-
-    widget_menu = QMenu(tool_button)
-    widget_menu.addAction(widget_action)
-
-    # Install event filter for the submenu (widget_menu)
-    widget_menu.installEventFilter(self)
-
-    tool_button.setMenu(widget_menu)
-
-    return tool_button
-
-def eventFilter(self, obj, event):
-    if event.type() == QEvent.KeyPress:
-        key = event.key()
-
-        # Handle key events within the submenu here
-        print(f'Key pressed in submenu: {key}')
-
-    return super().eventFilter(obj, event)
-
-
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QToolButton, QVBoxLayout, QLabel, QWidget, QMenu, QWidgetAction
-from PyQt5.QtCore import Qt, QEvent, QTimer, QPoint
+from PyQt5.QtWidgets import QApplication, QWidget, QToolButton, QMenu, QAction, QHBoxLayout
+from PyQt5.QtCore import Qt, QEvent
 
-class Example(QMainWindow):
+class ToolButtonExample(QWidget):
     def __init__(self):
         super().__init__()
 
         self.initUI()
-        self.current_tool_button = None  # Track the currently focused QToolButton
 
     def initUI(self):
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
+        layout = QHBoxLayout(self)
 
-        layout = QVBoxLayout(central_widget)
+        tool_button1 = QToolButton()
+        tool_button1.setText("ToolButton 1")
+        tool_button1.clicked.connect(self.showMenu)
+        tool_button1.mousePressEvent = self.toolButtonMousePressEvent  # Override mousePressEvent
+        layout.addWidget(tool_button1)
 
-        # Create QToolButtons
-        self.tool_buttons = [self.createToolButton(f'ToolButton {i + 1}') for i in range(2)]
-        for tool_button in self.tool_buttons:
-            layout.addWidget(tool_button)
+        tool_button2 = QToolButton()
+        tool_button2.setText("ToolButton 2")
+        tool_button2.clicked.connect(self.showMenu)
+        tool_button2.mousePressEvent = self.toolButtonMousePressEvent  # Override mousePressEvent
+        layout.addWidget(tool_button2)
 
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('QToolButton Navigation Example')
-        self.show()
+        self.setLayout(layout)
 
-    def createToolButton(self, label):
-        tool_button = QToolButton()
-        tool_button.setText(label)
-        tool_button.setPopupMode(QToolButton.InstantPopup)
+        self.setWindowTitle('QToolButton Example')
+        self.setGeometry(300, 300, 300, 100)
 
-        widget = QWidget()
-        widget_layout = QVBoxLayout(widget)
-        widget_label = QLabel(f'Additional Label for {label}')
+    def showMenu(self):
+        sender_button = self.sender()
+        if sender_button:
+            menu = QMenu(self)
+            menu.addAction(QAction("Action 1", self))
+            menu.addAction(QAction("Action 2", self))
+            menu.addAction(QAction("Action 3", self))
 
-        # Add space between widgets
-        widget_layout.addSpacing(10)
+            action = menu.exec_(sender_button.mapToGlobal(sender_button.rect().bottomLeft()))
+            if action:
+                print(f"Selected action: {action.text()}")
 
-        # Create a grid of buttons (just for demonstration)
-        for i in range(3):
-            for j in range(3):
-                button = QToolButton()
-                button.setText(f'{i * 3 + j + 1}')
-                widget_layout.addWidget(button)
+    def toolButtonMousePressEvent(self, event):
+        print("QToolButton clicked by mouse")
 
-        widget_layout.addWidget(widget_label)
-
-        widget_action = QWidgetAction(tool_button)
-        widget_action.setDefaultWidget(widget)
-
-        widget_menu = QMenu(tool_button)
-        widget_menu.addAction(widget_action)
-        tool_button.setMenu(widget_menu)
-
-        tool_button.installEventFilter(self)  # Install event filter for QToolButton
-
-        return tool_button
+        super().mousePressEvent(event)
 
     def eventFilter(self, obj, event):
-        if event.type() == QEvent.FocusIn and obj in self.tool_buttons:
-            self.current_tool_button = obj
-            self.current_tool_button.setStyleSheet("background-color: yellow")
-            return True
-        return False
+        if event.type() == QEvent.KeyPress:
+            key = event.key()
+            print(f'Key pressed: {key}')
+            return True  # Consume the event
 
-    def keyPressEvent(self, event):
-        key = event.key()
-
-        # Navigate between QToolButtons using left/right arrows
-        if key == Qt.Key_Left:
-            self.tool_buttons[0].setStyleSheet("background-color: yellow")
-            self.tool_buttons[1].setStyleSheet("")
-            self.navigate(-1)
-        elif key == Qt.Key_Right:
-            self.tool_buttons[0].setStyleSheet("")
-            self.tool_buttons[1].setStyleSheet("background-color: yellow")
-            self.navigate(1)
-
-        # Show the layout when pressing the Enter key
-        elif key == Qt.Key_Enter or key == Qt.Key_Return:
-            print("OPEN")
-            self.showLayout()
-
-    def navigate(self, direction):
-        if self.current_tool_button in self.tool_buttons:
-            current_index = self.tool_buttons.index(self.current_tool_button)
-            new_index = (current_index + direction) % len(self.tool_buttons)
-
-            # Remove highlight from the current tool button
-            self.current_tool_button.setStyleSheet("")
-
-            # Set focus on the new tool button
-            self.tool_buttons[new_index].setFocus()
-
-            # Highlight the new tool button (for demonstration purposes)
-            self.tool_buttons[new_index].setStyleSheet("background-color: yellow")
-
-    def showLayout(self):
-        print("show")
-        if self.current_tool_button:
-            print("show2")
-            # Simulate a mouse press event to open the menu and show the layout
-            point = self.current_tool_button.rect().center()
-            global_point = self.current_tool_button.mapToGlobal(point)
-            mouse_event = QEvent(QEvent.MouseButtonPress)
-            mouse_event.setPos(QPoint(global_point.x(), global_point.y()))
-            QApplication.postEvent(self.current_tool_button, mouse_event)
+        return super().eventFilter(obj, event)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    example = ToolButtonExample()
+    example.installEventFilter(example)
+    example.show()
     sys.exit(app.exec_())
